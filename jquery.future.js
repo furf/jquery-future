dab4945be31e4a8dcc1ff53cb1de881e74a063b2
Dave Furfero <furf@furf.com>
2012-02-22 15:26:12 -0500

    second

---
+++ jquery.future.js
@@ -0,0 +1,49 @@
+/*!
+ * jQuery.future
+ */
+!function ($, future, fn, name, method) {
+
+  fn = $.fn;
+
+  // For every...
+  for (name in fn) {
+
+    // ...function in $.fn...
+    if (fn.hasOwnProperty(name) && $.isFunction((method = fn[name]))) {
+
+      // ...create a mapped function...
+      future[name] = function (method) {
+
+        // ...that wraps the original function...
+        return function () {
+          
+          var args = arguments;
+
+          // ...in a pipe...
+          return this.pipe(function (collection) {
+            
+            // ...for execution...
+            return method.apply(collection, args);
+            
+          // ... at a later time
+          // (wrapping the promise around this is the deferred way to chain!)
+          }).promise(this);
+        };
+
+      }(method);
+    }
+  }
+  
+  $.future = function (deferred) {
+
+    // When the deferred is resolved...
+    return $.when(deferred)
+
+      // ...create a jQuery instance...
+      .pipe($)
+
+      // ...that will be used as the context for calls chained to the future
+      .promise(future);
+  };
+
+}(jQuery, {});
